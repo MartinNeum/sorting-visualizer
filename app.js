e6533c40fd412ddc937c67a95c18a7fc842b7a98
Martin Neumann <mart.neumann@ostfalia.de>
2021-10-26 13:12:23 +0200

    Added 'New Array'-Button

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-function addBox() {
+function createNewArray() {
+    // Delete old Array
+    deleteArray();
 
-    for(var i=0; i<10; i++) {
-        var randomNumber = Math.floor(Math.random()*11);
+    // Create Array with 10 Boxes with random number from 0 - 10
+    for(let i=0; i<10; i++) {
+        let randomNumber = Math.floor(Math.random()*11);
         document.getElementById("app").innerHTML += `<div class="box_container">` + randomNumber + `</div>`;
     }
+}
 
+function deleteArray() {
+    const boxCollection = document.getElementsByClassName("box_container");
+    let laenge = boxCollection.length;
+
+    for (let i = 0; i < laenge; i++) {
+        boxCollection[0].parentNode.removeChild(boxCollection[0]);
+    }
 }
