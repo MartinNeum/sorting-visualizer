565b9cacdc37b35d07a839a011db6331dcb26c93
Martin Neumann <mart.neumann@ostfalia.de>
2021-11-10 11:07:23 +0100

    Added code documentation

--- mergeSortButton.js
+++ mergeSortButton.js
@@ -1,46 +1,61 @@
 function mergeSort(box_containers) {
-    // Starts Merge Sort Algorithm.
-    // First, all boxes are splitted until they stand alone. Secondly, the boxes get sorted while merging them
+    /**
+     * Starts the Merge-Sort Algorithm. First, all boxes are splitted recursively until they stand alone.
+     * Secondly, the boxes get merged together recursively while sorting them.
+     *
+     * @param box_containers Array with all boxes on the website
+     */
 
-    // Split up the box-array to single pieces into 'left'-array and 'right'array
     if(box_containers.length > 1) {
-        let middle = Math.floor(box_containers.length / 2);
-        const left = [];
-        const right = [];
+        const splitted = split(box_containers);
+        const left = [...splitted[0]];
+        const right = [...splitted[1]];
 
-        for (let i = 0; i < box_containers.length; i++) {
-            box_containers[i].style.backgroundColor = 'green';
-        }
-        sleep(500);
-
-
-        // Save 'left' boxes
-        for (let i = 0; i < middle; i++) {
-            left.push(box_containers[i]);
-        }
-
-        // Save 'right' boxes
-        for (let i = middle; i < box_containers.length; i++) {
-            right.push(box_containers[i]);
-        }
-
-        // Recursive split until only one box remains
-        mergeSort(left);
+        mergeSort(left); // Recursive split
         mergeSort(right);
 
-        // Recursive merge until all boxes are sorted and merged together
-        merge(left, right);
+        merge(left, right); // Recursive merge
     }
 
-    applyStyle(box_containers);
+    applyStyle(box_containers); // Setup style for changed boxes
+}
+
+function split(box_containers) {
+    /**
+     * Split up the box-array to single pieces into 'left'-array and 'right'array.
+     * Is called recursively from mergeSort().
+     *
+     * @param box_containers Array with boxes which should get splitted
+     * @return Array Returns a 2-dimensional Array with left- and the right- side
+     */
+
+    let middle = Math.floor(box_containers.length / 2);
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < middle; i++) {
+        left.push(box_containers[i]);
+    }
+
+    for (let i = middle; i < box_containers.length; i++) {
+        right.push(box_containers[i]);
+    }
+
+    return [left, right];
 }
 
 function merge(left, right) {
+    /**
+     * Merges boxes to one Array, which are provided in the 'left' and 'right' Array. The algortihm starts with the
+     * first box in each array. If the right box.value is higher than the left box.value, it will get moved behind
+     * position of the left box. Is called recursively by mergeSort().
+     *
+     * @param left,right Arrays with boxes included, which should get merged together
+     */
+
     let pointerLeft = 0;
     let pointerRight = 0;
 
-    // Lookup, if value form 'right' is higher as value from 'left'.
-    // If its true, set the lower value on right array-position.
     while (pointerLeft < left.length && pointerRight < right.length){
         if(Number(left[pointerLeft].textContent) < Number(right[pointerRight].textContent)) {
             pointerLeft++;
@@ -69,7 +84,15 @@ function merge(left, right) {
 }
 
 function insertHigherValueOnCorrectPosition(pointerLeft, pointerRight, left, right) {
-    // Move remaining boxes between pointers one position to the right.
+    /**
+     * Puts the boxes in the right position during the merge-process. The right box gets to the position of the left
+     * box. All boxes, rangeing from the left box.position to right box.position get moved one position to the right.
+     *
+     * @param pointerLeft Position of the left box.
+     * @param pointerRight Position of the right box.
+     * @param left Array with all boxes on the left side
+     * @param right Array with all boxes on the right side
+     */
 
     let move_value = Number(right[pointerRight].textContent);
 
